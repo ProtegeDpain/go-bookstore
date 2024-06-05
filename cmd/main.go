86d053cff3189294f2328427cd5775843cb2a4f8
Chandrajeetsinh <chandrajeetsinh2003@gmail.com>
2024-06-05 23:37:58 +0530

    final

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ProtegeDpain/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
